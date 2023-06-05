77c040aeba2c2276bbef5ee5ce83353f389eef05
Shatilov Makar <m.point2011@yandex.ru>
2023-06-05 12:53:15 +0500

    google sheets instead of excel

--- ExcelExport.py
+++ ExcelExport.py
@@ -1,74 +1,33 @@
-import string
-import xlsxwriter
-from io import BytesIO
+import pygsheets
+from os.path import exists
+import streamlit as st
+import json
+
 
 class ExcelExport:
     '''
-        Класс для экспорта данных в excel
+        Класс для экспорта данных в google sheets
 
         Параметры:
-            df : датафрейм для экспорта в excel 
+            df : датафрейм для экспорта в google sheets 
     '''
+
     def __init__(self, df):
         self.__df = df
-        self.__headers_count = 19
-        self.__headers = ['Вх. дата', 'Номер увед-я', 'Дата увед-я','Форма реал-и', 'Вид имущ-ва', 'Имущество','Кол-во',
-                'Стоимость', 'Должник', 'Взыскатель', 'СПИ','Контактные данные СПИ, о/х',
-                'Дата истечения срока действия отчета об оценке','Дата уведомления о снижении',
-                'Дата посупления постановления на 15%','Статус залогового имущества',
-                'Дата пост-я о передаче','Дата пост-я об оценке','Фото','Доп информация']
+        if (~exists('client_secret.json')):
+            self.__create_client_secret()
+        gc = pygsheets.authorize("client_secret.json")
+        sheet = gc.open('my sheet')
+        self.workspace = sheet.sheet1
 
-    def export_to_excel(self):
-        output = BytesIO()
-        workbook = xlsxwriter.Workbook(output, {'in_memory': True})
-      #  workbook = xlsxwriter.Workbook('example.xlsx')
-        worksheet = workbook.add_worksheet()
-        self.__add_headers(worksheet,workbook)
-        self.__add_body(worksheet,workbook)
-        workbook.close()
-        return output
-    
-    def __add_headers(self, worksheet, workbook):
-        '''
-            Функция определяет заголовок таблицы
-        '''
-    
-        worksheet.set_column(0,5, 15) 
-        worksheet.set_column(5,5, 30) 
-        worksheet.set_column(6,18, 20)
+    def __create_client_secret(self):
+        service_account_info = {
+            'installed':
+                st.secrets['installed']
+        }
+        with open("client_secret.json", "w") as js:
+            json.dump(service_account_info, js, default=dict)
 
-        header_format = workbook.add_format()
-        header_format.set_bg_color('#00FFFF')
-        header_format.set_align('center')
-        header_format.set_align('vcenter')
-        header_format.set_border(1)
-        header_format.set_text_wrap()        
-        header_coords = [letter+'1' for letter in string.ascii_uppercase[:self.__headers_count]]
-
-        for header, coords in zip(self.__headers,header_coords):
-            worksheet.write(coords, header, header_format)
-
-
-    
-    def __add_body(self, worksheet,workbook):
-        '''
-            Функция определяет тело таблицы
-        '''
-        properties = [list(i.values()) for i in self.__df.to_dict(orient='records')]
-        columns_with_data=['B','C','F','G','H','I','J','K']
-        columns = string.ascii_uppercase[:self.__headers_count]
-        cell_format = workbook.add_format()
-        cell_format.set_align('center')
-        cell_format.set_align('vcenter')
-        cell_format.set_border(1)
-        cell_format.set_text_wrap()
-        for i, prop_items in enumerate(properties):
-            for col in columns: #zip(columns_with_data, prop_items.values()):
-                coords = f'{col}{i+2}'
-                if col in columns_with_data:
-                    worksheet.write(coords, prop_items.pop(0), cell_format)
-                else:
-                    worksheet.write(coords, '', cell_format)
-
-
-
+    def export_to_google_sheet(self):
+        counts = int(self.workspace[1][1]) + 3
+        self.workspace.set_dataframe(self.__df, (counts, 1), copy_head=False)
--- index.py
+++ index.py
@@ -6,21 +6,31 @@ from ExcelExport import ExcelExport
 import pdf2image
 import aspose.words as aw
 import io
+import pygsheets
+import json
+import pandas as pd
+if 'file_index' not in st.session_state:
+    st.session_state['file_index'] = 0
 
+if 'files_count' not in st.session_state:
+    st.session_state['files_count'] = 0
 
-but = False
 def load_image():
     label_text = 'Выберите фотографию документа для распознавания'
     allowed_files = ['jpeg', 'jpg','png','pdf']
-    uploaded_file = st.file_uploader(label=label_text, type=allowed_files)
-    if uploaded_file is not None:
+    uploaded_files = st.file_uploader(
+        label=label_text, type=allowed_files, accept_multiple_files=True)
+    if uploaded_files:
+        st.session_state['files_count'] = len(uploaded_files)
+        current_file = [uploaded_file for i, uploaded_file in enumerate(uploaded_files)
+                        if i == st.session_state['file_index']][0]
         image_data = ''
-        if uploaded_file.type == "application/pdf":
+        if current_file.type == "application/pdf":
             try:
-                images = pdf2image.convert_from_bytes(uploaded_file.read())
+                images = pdf2image.convert_from_bytes(current_file.read())
                 st.image(images[0], use_column_width=True)
 
-                doc = aw.Document(uploaded_file)
+                doc = aw.Document(current_file)
                 page = doc.extract_pages(0, 1)
                 buf = io.BytesIO()
                 saveOptions = aw.saving.ImageSaveOptions(aw.SaveFormat.JPEG)
@@ -31,7 +41,7 @@ def load_image():
                 st.warning('**Не удалось отбразить документ**')
                 st.stop()
         else:
-            image_data = uploaded_file.getvalue()
+            image_data = current_file.getvalue()
             st.image(image_data)
         return image_data
     else:
@@ -50,15 +60,23 @@ def get_recognized_data(img):
     return jsonParser,df
 
 @st.cache_data(show_spinner=False,ttl=600) 
-def get_excel_data(df, notif_number, notif_date, debtor, claimant, officer):
-    df =  df[( df['property'].str.lower().str.find('итого') < 0)]    
-    df.insert(0, "notif_number", notif_number)
-    df.insert(1, "notif_date", notif_date)
-    df.loc[:,'debtor' ] = debtor
-    df.loc[:,'claimant' ] = claimant
-    df.loc[:,'officer' ] = officer
-    excel = ExcelExport(df)
-    return excel.export_to_excel()
+def to_sheet(df, notif_number, notif_date, debtor, claimant, officer):
+    try:
+        df =  df[( df['property'].str.lower().str.find('итого') < 0)]    
+        df.insert(0, "notif_number", notif_number)
+        df.insert(1, "notif_date", notif_date)
+        df.loc[:,'debtor' ] = debtor
+        df.loc[:,'claimant' ] = claimant
+        df.loc[:,'officer' ] = officer
+        excel = ExcelExport(df)
+        excel.export_to_google_sheet()
+        return 'OK'
+    except:
+        return 'ERROR'
+    
+
+
+
 
 if img:
     with st.spinner('Идет обработка...'):
@@ -78,14 +96,24 @@ if img:
             debtor= st.text_input('Должник',ner.get_debtor_name())
             claimant = st.text_input('Взыскатель',ner.get_claimant())
 
-            excel_data = get_excel_data(df,notif_number, notif_date,debtor,claimant,officer_name)
 
-            st.download_button(
-                label="Сгенерировать отчет",
-                data=excel_data.getvalue(),
-                file_name=f"{notif_number}.xlsx",
-                mime="application/vnd.ms-excel"
-            )
+            to_excel = st.button('Добавить в реестр',type="primary")
+            if to_excel:
+                result = to_sheet(df, notif_number, notif_date, debtor, claimant, officer_name)
+                if 'ОК':
+                    st.success('**Добавлено в реестр!**')
+                else:
+                    st.warning('**Не удалось добавить документ в реестр**')
+
+
         except:
             st.warning('**Не удалось распознать документ**')
             st.stop()
+
+
+
+def flip_document():
+    st.session_state['file_index'] += 1
+    
+if (st.session_state['file_index'] < st.session_state['files_count'] - 1):
+   st.button('Далее', on_click=flip_document)
